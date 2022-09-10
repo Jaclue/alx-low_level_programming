7c3cb120b6113bef4f3d7878c9b9bd76f57ce3f6
jaclue <jaclue@example.com>
2022-09-10 03:48:23 -0700

    6 size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,8 +15,7 @@ int main(void)
 	float afloat;
 
 	printf("size of a char: %1u byte(s)\n", sizeof(acharacter));
-	printf("size of an int: %1u byte(s)\n", sizeof(ainteger))
-;
+	printf("size of an int: %1u byte(s)\n", sizeof(ainteger));
 	printf("size of a long int: %1u byte(s)\n", sizeof(along));
 	printf("size of a long long int: %1u byte(s)\n", sizeof(alonglong));
 	printf("size of a float: %1u byte(s)\n", sizeof(afloat));
