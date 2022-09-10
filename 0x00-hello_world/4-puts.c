0cdc5cb0487952999424f3571c4a3c2a7d4c241e
jaclue <jaclue@example.com>
2022-09-10 00:35:21 -0700

    4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,11 @@
 
 /**
 * main - Entry point
+*
 * Return: Always 0 (sucess)
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");		return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
