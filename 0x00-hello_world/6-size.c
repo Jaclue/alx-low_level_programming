fe3b58c396cf36227250fb59415b614e6729e788
jaclue <jaclue@example.com>
2022-09-11 21:54:45 -0700

    6size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,13 +14,11 @@ int main(void)
 	long long alonglong;
 	float afloat;
 
-	printf("size of a char: %1u byte(s)\n", sizeof(acharacter));
-	printf("size of an int: %1u byte(s)\n", sizeof(ainteger));
-	printf("size of a long int: %1u byte(s)\n", sizeof(along));
-	printf("size of a long long int: %1u byte(s)\n", sizeof(alonglong));
-	printf("size of a float: %1u byte(s)\n", sizeof(afloat));
+	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
+	printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
+	printf("Size of a float: %lu byte(s)\n", sizeof(afloat);
 
 	return (0);
 }
-
-
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -1,1 +1,1 @@
-Size is not grandeur, and territory does not make a nation
+ grandeur, and territory does not make a nation
