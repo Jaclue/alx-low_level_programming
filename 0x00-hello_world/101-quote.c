90c7b7b16dff7939d6f377146463566eda25ea0a
jaclue <jaclue@example.com>
2022-09-11 22:58:57 -0700

    101 quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,4 +6,10 @@
  *
  * Description: 'and that piece of art is useful" - Dora Korpar, 2015-10-19'
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
