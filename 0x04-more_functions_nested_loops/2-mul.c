056bc0583fce820b8fd759e72e47c9b24737a6cc
jaclue <jaclue@example.com>
2022-09-15 17:11:41 -0700

    moreNestedLoop

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - function that multiplies 2 integrals
+ * @a: first integral
+ * @b: second integral
+ * Return: variable x
+ */
+int mul(int a, int b)
+{
+	int x;
+
+	x = a * b;
+	return (x);
+}
